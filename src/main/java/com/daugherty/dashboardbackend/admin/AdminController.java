a78251645b03ae1d335cfab1f0815da198b0709f
Josh Auer <joshua.auer@daugherty.com>
2019-04-15 13:52:14 -0400

    Adding CORS handling

--- src/main/java/com/daugherty/dashboardbackend/admin/AdminController.java
+++ src/main/java/com/daugherty/dashboardbackend/admin/AdminController.java
@@ -3,14 +3,12 @@ package com.daugherty.dashboardbackend.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+@CrossOrigin
 @RestController
 public class AdminController {
 
